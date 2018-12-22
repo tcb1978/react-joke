093c92d9a015e5f10e862a259fba6b8db5923b7e
Matthew Eldredge <mattheweldredge@gmail.com>
2018-12-22 15:31:49 -0800

    make jokes dynamic and from jokes data, refactor

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React, { Component } from 'react';
 import Joke from './Components/Joke/Joke';
+import jokesData from './Components/Joke/JokesData';
 
 export default class App extends Component {
   render() {
+    const jokesComponent = jokesData.map( joke => <Joke question={joke.question} punchLine={joke.punchLine} key={joke.id} />);
     return (
       <div className="App">
-      <Joke
-          question="What's the best thing about Switzerland?"
-          punchLine="I don't know, but the flag is a big plus!"
-      />
-
-      <Joke
-          question="Did you hear about the mathematician who's afraid of negative numbers?"
-          punchLine="He'll stop at nothing to avoid them!"
-      />
-
-      <Joke
-          question="Hear about the new restaurant called Karma?"
-          punchLine="There’s no menu: You get what you deserve."
-      />
-
-      <Joke
-          question="Did you hear about the actor who fell through the floorboards?"
-          punchLine="He was just going through a stage."
-      />
+        {jokesComponent}
       </div>
     );
   }
