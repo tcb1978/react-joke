f9690fbd843965a45ccbff630ff415ebdb91a491
Matthew Eldredge <mattheweldredge@gmail.com>
2018-12-22 14:56:24 -0800

    since h3 already has a default display refactor condition

--- src/Components/Joke/Joke.js
+++ src/Components/Joke/Joke.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Joke = (props) => {
   return (
     <div>
-      <h3 style={{ display: props.question ? "block" : "none" }}>Question: {props.question}</h3>
-      <h3 style={{ display: props.punchLine ? "block" : "none" }}>Answer: {props.punchLine}</h3>
+      <h3 style={{ display: !props.question && "none" }}>Question: {props.question}</h3>
+      <h3 style={{ display: !props.punchLine && "none" }}>Answer: {props.punchLine}</h3>
     </div>
   )
 }
